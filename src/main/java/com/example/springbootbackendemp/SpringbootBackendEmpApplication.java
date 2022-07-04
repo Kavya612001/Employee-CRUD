e6e8f0617fa1b7c02956d2ca0aae512392c69447
Kavya <71892643+Kavya612001@users.noreply.github.com>
2022-07-04 19:01:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/springbootbackendemp/SpringbootBackendEmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbackendemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendEmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootbackendemp/SpringbootBackendEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbackendemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
